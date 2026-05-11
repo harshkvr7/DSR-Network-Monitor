b4f42a86c08ece73b5689722a034fbd5e4c9012d
harshkvr7 <darshnar34@gmail.com>
2026-05-11 14:35:31 +0530

    Add GitHub Action for Windows build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  worker: {
-    format: 'es',
+  // Prevent Vite from obscuring Rust errors
+  clearScreen: false,
+  // Tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 5173,
+    strictPort: true,
   },
+  // Ensure the build environment variables are exposed
+  envPrefix: ['VITE_', 'TAURI_'],
 })
